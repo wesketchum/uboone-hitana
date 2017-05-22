f57ccdfeda0efd4ff060eab2695bacc109e0fbad
David Caratelli <devish.c@gmail.com>
2017-05-22 13:58:54 -0500

    script to make all plots at once + README --DC

---
+++ python/README.txt
@@ -0,0 +1,4 @@
+Produce plots by running:
+
+source setup_python.sh
+python make_plots.py ttree_file.root
---
+++ python/make_plots.py
@@ -0,0 +1,37 @@
+import sys,os
+
+if (len(sys.argv) != 2):
+
+    print
+    print 'Run as:'
+    print 'python make_plots.py INPUT_ROOT_TTREE_FILE'
+    print
+    sys.exit()
+
+inputfile = sys.argv[-1]
+
+if (os.path.isfile(inputfile) == False):
+    print
+    print 'input file %s not a valid file'%inputfile
+    print
+    sys.exit()
+
+if (inputfile.find('.root') < 0): 
+    print
+    print 'input file %s needs to be a valid ROOT file'%inputfile
+    print
+    sys.exit()
+
+cmd = 'python SummedADC_vs_Integral.py %s'%inputfile
+print
+print 'RUNNING %s'%cmd
+print
+os.system(cmd)
+
+cmd = 'python HitGoF.py %s'%inputfile
+print
+print 'RUNNING %s'%cmd
+print
+os.system(cmd)
+
+sys.exit()
