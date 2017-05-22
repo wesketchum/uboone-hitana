9edc26d9a4f40ad0badaec6e6962c4dde0c265d4
David Caratelli <devish.c@gmail.com>
2017-05-22 17:10:15 -0500

    expanding setup_python.sh --DC

--- python/setup_python.sh
+++ python/setup_python.sh
@@ -1,1 +1,5 @@
+source /grid/fermiapp/products/uboone/setup_uboone.sh
+
+setup uboonecode v06_26_01_03 -q e10:prof
+
 export PYTHONPATH=/uboone/app/users/wketchum/python_install/lib/python2.7/site-packages:$PYTHONPATH
