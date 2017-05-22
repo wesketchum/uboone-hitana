838b9ae4a32a5a27e241c5ad15608ce2f52f4aba
David Caratelli <devish.c@gmail.com>
2017-05-22 10:18:16 -0500

    adding python script folder --DC

---
+++ python/example.py
@@ -0,0 +1,10 @@
+import sys,os
+
+try:
+    import matplotlib.pyplot as plt
+    import numpy as np
+else:
+    print 'Cannot import matplotlib or numpy'
+    print 'EXIT '
+    print
+    sys.exit()
